764de2052cf69aaced49f6ff90e2e73000ea3c94
Allen Wipf <allenwipf@msn.com>
2017-02-11 08:41:14 -0600

    finished exercises

---
+++ exercises/global.js
@@ -0,0 +1,126 @@
+window.addEventListener("load", function(){
+
+	function addMoreListener(){
+
+		document.getElementById("more_text_link").addEventListener("click", secondP)
+	}
+
+	function hideAllListener(){
+
+		document.getElementById("button").addEventListener("click", showAll)
+	}
+
+	function toggleManyListener(){
+
+		document.getElementById("button").addEventListener("click", toggleMany)
+	}
+
+	function toggleElementsListener(){
+
+		document.getElementById("toggle_button").addEventListener("click", toggleElements)
+	}
+
+	function detectTypeListener(){
+
+		document.getElementById("full_name").addEventListener("input", detectType)
+	}
+
+	function autoTabListener(){
+
+		document.getElementById("first").addEventListener("input", autoTab)
+		document.getElementById("second").addEventListener("input", autoTab)
+	}
+
+	// ENABLE THE LISTENER FOR THE PAGE BEING TESTED
+
+	// addMoreListener()
+	// hideAllListener()
+	// toggleManyListener()
+	// toggleElementsListener()
+	// detectTypeListener()
+	autoTabListener()
+  
+});
+
+// excercise 1. show or hides the second paragraph after clicking "More..."
+function secondP(){
+	secondP = document.getElementById("more_text_content").style.display
+
+	if (secondP == "block") {
+		document.getElementById("more_text_content").style.display = "none"
+
+	} else {
+		document.getElementById("more_text_content").style.display = "block"
+	}
+}
+
+// exercise 2 & 3. shows and hides lines of text on the 02-hide-many-elements.html page. 
+function showAll(e){
+	list = document.getElementsByClassName("hide_me")
+
+	for (var i = 0; i <= list.length -1; i ++){
+
+		if (list[i].style.display == "block") {
+			list[i].style.display = 'none'
+
+		} else {
+			list[i].style.display = 'block'
+		}
+	}
+}
+
+// exercise 4. show and hide lines of text on 04-toggle-many-elements.html
+function toggleMany(){
+	list = document.getElementsByClassName("toggle_me")
+
+	for (var i = 0; i <= list.length - 1; i++){
+
+		if (list[i].style.display == "block") {
+			list[i].style.display = "none"
+		
+		} else {
+			list[i].style.display = "block"
+		}
+	}
+}
+
+// exercise 5. show and hide a specific list
+function toggleElements(){
+	list = document.getElementsByClassName("second_five")[0].getElementsByTagName("li")
+
+	for (var i = 0; i <= list.length - 1; i++){
+
+		if (list[i].style.display == "block") {
+			list[i].style.display = "none"
+		
+		} else {
+			list[i].style.display = "block"
+		}
+	}
+}
+// exercise 6. outputs typed text on page.
+function detectType(){
+
+	typed = document.getElementById("full_name").value
+
+	document.getElementById("greeting").innerText = typed
+}
+
+// exercise 7. tabs from on input box to the next
+function autoTab(){
+
+	if ((this.id == "first") && ((this.value).length == 3)){
+
+		document.getElementById("second").focus()
+
+	} else if ((this.id == "second") && ((this.value).length == 3)){
+		document.getElementById("third").focus()
+	}
+}
+
+
+
+
+
+
+
